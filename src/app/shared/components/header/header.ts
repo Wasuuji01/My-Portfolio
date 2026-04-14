cd21c9cde7220091e8d1898624301895aa4ff4d3
Wasuuji01 <159437021+Wasuuji01@users.noreply.github.com>
2026-04-14 22:48:24 +0530

    Initial commit for my portfolio

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // angular module for ngClass
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule], // ✅ COMMONMODULE IS IMPORTANT for ngClass
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
 export class HeaderComponent {
-  
-  constructor() {}
+  // ✅ Create a variable to track the state of the mobile menu
+  menuOpen: boolean = false;
 
+  // ✅ Add a function to toggle the menu (Open -> Close, Close -> Open)
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
